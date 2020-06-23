1c76e2bc66c338e1eebdfafe34810f50e0b23cf8
sulmandvlpr <sulmanzafa@gmail.com>
2020-06-23 17:35:50 +0500

    commet test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>by Sulman!!</code> finally done
+          Edit <code>by Sulman!!!</code> finally done
         </p>
         <a
           className="App-link"
